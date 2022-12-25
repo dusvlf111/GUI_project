8447319ff75d536de9be9acb915434de773c13ae
dusvlf5950 <dusvlf5950@naver.com>
2022-12-25 18:51:56 +0900

    2022 1225 0651

---
+++ project/2_basic_funcion.py
@@ -0,0 +1,150 @@
+from tkinter import (
+    Tk, Frame, Button, Scrollbar, Listbox,
+    LabelFrame ,Entry, Label, DoubleVar, filedialog,
+    END, messagebox 
+)
+import tkinter.ttk as ttk
+
+root = Tk()
+root.title("sungbin GUI")
+
+#함수
+#   - 파일
+#       - 파일추가
+def add_file():
+    files = filedialog.askopenfilenames(
+        title='이미지 파일을 선택하세요',
+        filetypes=(
+            ('PNG파일', '*.png'), ('JPG파일', '*.jpg'), ('모든파일', '*.*')
+            ),
+        initialdir=r'C:\Users\a\Pictures'#최초의 c:/ 경로를 보여줌 
+        )
+    for file in files:  #사용자가 선택한 파일 목록
+        list_file.insert(END, file)
+          
+#       - 파일삭제
+def del_file():
+    #list_file.curselection().remove
+    for index in reversed(list_file.curselection()):
+        list_file.delete(index)    
+        #reverse 는 리스트의 숫자를 큰수부터 정렬 
+        #reversed 는 실제값에는 영향을 끼지치 않음
+
+#   저장 경로
+def browser_dest_path():
+    folder_selected = filedialog.askdirectory()
+    if folder_selected is None:
+        return
+    txt_dest_path.delete(0, END)
+    txt_dest_path.insert(0, folder_selected)
+
+#   시작버튼
+def start_cmd():
+    #각 옵션들 값을 확인
+    print('가로넓이 : ', cmb_width.get())
+    print('간격 : ', cmb_space.get())
+    print('포맷 : ', cmb_format.get())
+    
+    #파일 목록
+    if list_file.size() == 0:
+        messagebox.showwarning('경고', '이미지 파일을 추가하세요')
+        return
+    
+    #저장 경로 확인
+    if len(txt_dest_path.get()) == 0:
+        messagebox.showwarning('경고', '저장 경로를 선택하세요')
+        
+    
+    
+    
+#파일 프레임 (파일 추가, 선택 삭제)
+file_frame = Frame(root)
+file_frame.pack(fill='x', padx=5, pady=5) # 간격 띄우기
+#   - 파일추가 버튼
+btn_add_file = Button(file_frame, padx=5, pady=5, width=12, text="파일추가", command=add_file)
+btn_add_file.pack(side= "left", padx=5, pady=5)
+#   - 선택삭제 버튼
+btn_del_file = Button(file_frame, padx=5, pady=5, width=12, text="선택삭제", command=del_file)
+btn_del_file.pack(side= "right", padx=5, pady=5)
+
+
+# 리스트 프레임
+list_frame = Frame(root)
+list_frame.pack(fill="both", padx=5, pady=5)
+#   - 스크롤바
+scrollbar = Scrollbar(list_frame)
+scrollbar.pack(side="right", fill="y", padx=5, pady=5)
+#   - 리스트 박스
+list_file = Listbox(list_frame, selectmode="extended", height=15, yscrollcommand=scrollbar.set)
+list_file.pack(side="left", fill="both", expand=True)
+scrollbar.config(command=list_file.yview)
+
+# 저장 경로 
+#   - 프레임
+path_frame = LabelFrame(root, text="저장경로")
+path_frame.pack(fill='x', padx=5, pady=5, ipady=5)
+#   - 텍스트박스
+txt_dest_path = Entry(path_frame)
+txt_dest_path.pack(side="left", fill="x", expand=True, ipady=4, padx=5, pady=5) # 높이 변경
+#   - 버튼  
+btn_dest_path = Button(path_frame, text="찾아보기", width=10, command=browser_dest_path)
+btn_dest_path.pack(side="right", padx=5, pady=5)
+
+#옵션 프레임
+frame_option = LabelFrame(root, text="옵션")
+frame_option.pack(padx=5, pady=5, ipady=5)
+
+#   1.가로 높이 옵션
+#       - 가로높이 레이블
+lbl_width = Label(frame_option, text='가로넓이', width=8)
+lbl_width.pack(side='left', padx=5, pady=5)
+#       - 가로높이 콤보
+opt_width =  ['원본유지', '1024', '800', '640']
+cmb_width = ttk.Combobox(frame_option, state='readonly', values=opt_width, width=10)
+cmb_width.current(0)
+cmb_width.pack(side='left', padx=5, pady=5)
+
+#   2.간격옵션
+#       - 레이블
+lbl_space = Label(frame_option, text='간격', width=8)
+lbl_space.pack(side='left')
+#       - 콤보
+opt_space =  ['없음', '쫍게', '보통', '넓게']
+cmb_space = ttk.Combobox(frame_option, state='readonly', values=opt_space, width=10)
+cmb_space.current(0)
+cmb_space.pack(side='left', padx=5, pady=5)
+
+
+#   3.파일 포맷 옵션
+#       - 레이블
+lbl_format = Label(frame_option, text='포맷', width=8)
+lbl_format.pack(side='left', padx=5, pady=5)
+#       - 콤보
+opt_format =  ['PNG', 'JPG', 'BMP']
+cmb_format = ttk.Combobox(frame_option, state='readonly', values=opt_format, width=10)
+cmb_format.current(0)
+cmb_format.pack(side='left', padx=5, pady=5)
+
+#진행상황 progress bar
+#   - 프레임
+frame_progress = LabelFrame(root, text='진행상황')
+frame_progress.pack(fill='x', padx=5, pady=5, ipady=5)
+#   - 프로그레스 바
+p_var = DoubleVar
+progress_bar = ttk.Progressbar(frame_progress, maximum=100, variable=p_var)
+progress_bar.pack(fill='x', padx=5, pady=5)
+
+# 실행 프레임
+frame_run = Frame(root)
+frame_run.pack(fill='x', padx=5, pady=5)
+#   -닫기 버튼
+btn_close = Button(frame_run, padx=5, pady=5, text='닫기', width=12, command=root.quit)
+btn_close.pack(side='right', padx=5, pady=5)
+#   -시작 버튼
+btn_start = Button(frame_run, padx=5, pady=5, text='시작', width=12, command=start_cmd)
+btn_start.pack(side='right', padx=5, pady=5)
+
+
+#--
+root.resizable(False, False)
+root.mainloop()
